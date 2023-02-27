9d66398d284b1bc3748022bd5190ab45b526a980
Srushti Sonavane <96761322+Srushti-S@users.noreply.github.com>
2023-02-27 17:04:32 +0530

    main files

---
+++ About_Me.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Hello Guysss :wave: I am :blue[Srushti Sonavane]')
+
+st.subheader("I am a junior pursuing B.E. in Computer Engineering. I am interested in problem-solving with an eye toward the fascinating world of Artificial Intelligence and I have a good understanding of Python and C++. Being an AI Enthusiast :sunglasses: I am here to learn and explore more in this domain!")
+
+st.header("You can connect with me through :handshake: :")
+
+st.write(":safety_pin: [Github](https://github.com/Srushti-S)")
+
+st.write(":pushpin: [Linkedin](https://www.linkedin.com/in/srushti-sonavane-9154151ba/)")
+
+st.write(":e-mail: [Email](srushtisonavane@gmail.com)")
